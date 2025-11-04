9f9fab8b5e8fe40c4814ae2743e35daefa7dc7b5
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2025-11-05 04:02:31 +0530

    background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,32 @@
-// "use client";
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
 import { requireAuth } from "@/lib/auth-utils";
 import { caller } from "@/trpc/server";
 import { LogoutButton } from "./logout";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { toast } from "sonner";
 
-const page = async () => {
-  await requireAuth();
+const page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
 
-  const data = await caller.getUsers();
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      toast.success("Job Queued");
+    },
+  }));
   
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       protected page
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <div>
+        {JSON.stringify(data, null, 2)}
+      </div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>Create</Button>
       <LogoutButton />
     </div>
   );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,16 +1,20 @@
-import { z } from "zod";
 import { protectedProcedure, createTRPCRouter } from "../init";
 import prisma from "@/lib/db";
+import { inngest } from "@/inngest/client";
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    console.log({ userId: ctx.auth.user.id });
-
-    return prisma.user.findMany({
-      where: {
-        id: ctx.auth.user.id,
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "one@two.com",
       },
     });
-  }),
+
+    return { sucess: true, message: "Job Queued" };
+  })
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
